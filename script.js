ac45814abf87d67fe2539c020a8451047849283a
gowsigan31 <gowsigan31@hotmail.com>
2026-01-08 11:38:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // Initialize the map centered on Toronto and set zoom to 11
-const map = L.map('map').setView([43.6532, -79.3832], 11);
+var map = L.map('map').setView([43.6532, -79.3832], 13);
 
 // Add OpenStreetMap tiles
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
 // Optional: Add a marker for City Hall
 L.marker([43.6532, -79.3832]).addTo(map)
     .bindPopup('Toronto City Hall')
     .openPopup();
+
